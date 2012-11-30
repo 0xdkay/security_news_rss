b38c2abf97f2147a0c8c927f7f86894f49b52d18
Dongkwan Kim <insinoa@gmail.com>
2012-11-30 20:26:58 +0900

    make libraries as class file

--- lib/get_site.rb
+++ lib/get_site.rb
@@ -1,44 +1,52 @@
 require 'mechanize'
 require './db'
 
-agent = Mechanize.new
-db = DB.new
-
-page = agent.get "http://www.esecurityplanet.com"
-site = "http://www.esecurityplanet.com"
-page.search("//div[@class='article homepage']").each do |v|
-	category = v.search("div[@class='category']/p").text.strip
-
-	t = v.search("h2/a")
-	if not t.empty?
-		description = v.search("p")[1].text
-	else 
-		t = v.search("div/h3/a")
-		description = v.search('p')[2].text
-	end
-
-	link = t.attr('href').text
-	title = t.text
-	author = v.search("a[@class='home']").text
-	date = v.search("span[@class='publish-date']").text.strip
-
-	puts "category: #{category}"
-	puts "link: #{link}"
-	puts "title: #{title}"
-	puts "description: #{description}"
-	puts "author: #{author}"
-	puts "date: #{date}"
-	puts 
-	
-	db.insert(site, category, link, title, description, author, date)
-end
-
-
 =begin
  1. You have to construct your own parsing mechanism depending on each site.
  2. Please match the format of database so that we can combine data easily.
 	(you can see db.insert part)
 =end
+
+class Getsite
+	def initialize
+		@db = DB.new
+	end
+
+
+	def get_esecurity PRINT=nil
+		agent = Mechanize.new
+		page = agent.get "http://www.esecurityplanet.com"
+		site = "http://www.esecurityplanet.com"
+		page.search("//div[@class='article homepage']").each do |v|
+			category = v.search("div[@class='category']/p").text.strip
+
+			t = v.search("h2/a")
+			if not t.empty?
+				description = v.search("p")[1].text
+			else 
+				t = v.search("div/h3/a")
+				description = v.search('p')[2].text
+			end
+
+			link = t.attr('href').text
+			title = t.text
+			author = v.search("a[@class='home']").text
+			date = v.search("span[@class='publish-date']").text.strip
+
+			if PRINT
+				puts "category: #{category}"
+				puts "link: #{link}"
+				puts "title: #{title}"
+				puts "description: #{description}"
+				puts "author: #{author}"
+				puts "date: #{date}"
+				puts 
+			end
+			
+			@db.insert(site, category, link, title, description, author, date)
+		end
+	end
+end
 
 
 
--- lib/rss.rb
+++ lib/rss.rb
@@ -1,6 +1,7 @@
 require './db'
 require 'rss'
 
+# Database format is shown as below:
 =begin
 	CREATE TABLE #{@table_name} (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -16,29 +17,38 @@ require 'rss'
 ["id", "site", "category", "link", "title", "desc", "author", "date"]
 =end
 
-db = DB.new
-column_name = db.table_info.map {|v| v[1]}
-rss = RSS::Maker.make("2.0") do |maker|
-	maker.channel.title = "BoB Security News"
-	maker.channel.description = "This page shows latest security news"
-	maker.channel.link = "http://gon.kaist.ac.kr"
-	maker.channel.date = Time.now.to_s
-	maker.items.do_sort = true
+class Makerss
+	def initialize
+		@db = DB.new
+		db = DB.new
+		column_name = db.table_info.map {|v| v[1]}
+		rss = RSS::Maker.make("2.0") do |maker|
+			maker.channel.title = "BoB Security News"
+			maker.channel.description = "This page shows latest security news"
+			maker.channel.link = "http://gon.kaist.ac.kr"
+			maker.channel.date = Time.now.to_s
+			maker.items.do_sort = true
 
-	db.size.times do |i|
-		article = db.select(i+1)[0]
-		article.map!.with_index {|v, k| [column_name[k],v]}
-		article = Hash[article]
-		maker.items.new_item do |item|
-			item.link = article['site'] + article['link']
-			item.title = article['title']
-			item.description = article['desc']
-			item.author = article['author']
-			item.date = article['date']
+			#TODO: select latest news
+			db.size.times do |i|
+				article = db.select(i+1)[0]
+				article.map!.with_index {|v, k| [column_name[k],v]}
+				article = Hash[article]
+				maker.items.new_item do |item|
+					item.link = article['site'] + article['link']
+					item.title = article['title']
+					item.description = article['desc']
+					item.author = article['author']
+					item.date = article['date']
+				end
+			nd
+
+			#TODO: add this to server
+			open("/var/www/test/rss.html", "w"){|f| f.puts rss}
 		end
 	end
 end
-open("/var/www/test/rss.html", "w"){|f| f.puts rss}
+
 
 
 
